e133dbf537cea11396928385e9a20ec3c2b85424
HarshiniNalla <144764843+HarshiniNalla@users.noreply.github.com>
2026-05-21 17:48:20 +0530

    communication_with_var

--- print.sh
+++ print.sh
@@ -1,7 +1,7 @@
 #!bin/bash
-persion1=raju
+persion1=vijay
 persion2=srinu
 echo "$persion1: can i know your name"
-echo "$persion2: my name is $persion2, what is yur name"
+echo "$persion2: my name is $persion2, what is your name"
 echo "$persion2: my name is $persion1"
 
