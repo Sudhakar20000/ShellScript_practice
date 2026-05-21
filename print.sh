5ae216ed17868a14d1ccc0002c5c98cc7a1f7921
Sudhakar20000 <mandahemasudhakar@gmail.com>
2026-05-21 17:56:39 +0530

    runtime_aug

--- print.sh
+++ print.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 persion1=$1
 persion2=$2
-echo "$persion1: can i know your name"
+echo "$persion1: can i know your name."
 echo "$persion2: my name is $persion2, what is your name"
 echo "$persion1: my name is $persion1"
 
