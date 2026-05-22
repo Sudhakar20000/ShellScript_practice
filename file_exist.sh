32cb761cbb709d865c5094e9ea90b2bc2bd5349e
Sudhakar20000 <mandahemasudhakar@gmail.com>
2026-05-22 16:05:32 +0530

    file_exist

--- file_exist.sh
+++ file_exist.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-echo " give the name of the file:"
+
+echo "Give the name of the file:"
 read file
+
+# Log file for errors
 log="/root/error.log"
-found=$(find / -type f -name $file)
-if [ -n "$found" &>>"log" ]; then
-echo " file exists"
-echo "$found"
-else 
-echo "file not exists"
+
+# Search entire filesystem for the file, redirect permission errors to log
+found=$(find / -type f -name "$file" 2>>"$log")
+
+# Check if any file was found
+if [ -n "$found" ]; then
+    echo "File exists."
+    echo "Locations:"
+    echo "$found"
+else
+    echo "File does not exist."
 fi
