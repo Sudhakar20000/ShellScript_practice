4092c1179e595aab1b72289c0a7073b1ad027b71
Sudhakar20000 <mandahemasudhakar@gmail.com>
2026-05-22 15:59:34 +0530

    file_exist

--- file_exist.sh
+++ file_exist.sh
@@ -1,10 +1,10 @@
 echo " give the name of the file:"
 read file
-log=/root/error.log
+log="/root/error.log"
 found=$(find / -type f -name $file)
-if [ -n "$found" &>> log ]; then
+if [ -n "$found" &>> "log" ]; then
 echo " file exists"
+echo "$fond"
 else 
 echo "file not exists"
 fi
-echo " file locations $found"
