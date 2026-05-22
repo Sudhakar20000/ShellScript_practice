6891d8a39579809f9386558576eaa898c04d4d24
Sudhakar20000 <mandahemasudhakar@gmail.com>
2026-05-22 12:25:38 +0530

    Special_vars

---
+++ special_vars.sh
@@ -0,0 +1,11 @@
+echo "print all args $@"
+echo "print no.of augs $#"
+echo "print the script name $0"
+echo "print the current user $USER"
+echo "print the working directory $PWD"
+echo "print the PID of script $$"
+echo "print the echo "print the previous command success are not $?"
+sleep 3 &
+
+echo "print the previous command pid $!"
+echo "print the time to complete seript $SECONDS"
