66a2af1b7a3d7cf9cfa53b12acd049030f3acb06
Sudhakar20000 <mandahemasudhakar@gmail.com>
2026-05-22 12:14:32 +0530

    time_taken

--- calculate_time.sh
+++ calculate_time.sh
@@ -2,5 +2,5 @@
 start_time=$(date +%s)
 sleep 5 
 end_time=$(date +%s)
-totl_time=$(($end_time-start_time))
+total_time=$(($end_time-start_time))
 echo "total time taken to complete $total_time seconds"
