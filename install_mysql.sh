3468cc1fbd1785c229f0cd0b2130106f351f9eeb
Sudhakar20000 <mandahemasudhakar@gmail.com>
2026-05-22 12:58:37 +0530

    mysql_install

--- install_mysql.sh
+++ install_mysql.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
  #check the current user
   USER=$(id -u)
-  if [$USER -ne 0]; then
+  if [ $USER -ne 0 ]; then
     echo "run as root"
     exit 1
     fi
 #check if the mysql is exists
 mysql --version
-if [ $? -eq 0]; then
+if [ $? -eq 0 ]; then
 echo "mysql is already installed"
 exit 1
 fi
